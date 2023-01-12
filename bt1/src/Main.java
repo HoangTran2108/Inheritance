9e2d11a3b81c5203808b18d7f6d68edf765e7da7
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-01-12 16:28:46 +0700

    [Bài tập] Lớp Circle và lớp Cylinder

---
+++ bt1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3,"red");
+        System.out.println(circle);
+        
+        Cylinder cylinder = new Cylinder(3,"Yellow",5);
+        System.out.println(cylinder);
+    }
+}
